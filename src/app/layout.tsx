1858b17ecc1961a35ca8fd677d47e6907027d692
DenSovan <sarimramata@gmail.com>
2025-11-18 15:17:25 +0700

    add thumbnial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata, Viewport } from "next"
-
+import type { Metadata, Viewport } from "next";
+// import banner from '../../public/images/thumnailOpenGraph.png'
 import Footer from "@/components/ui/footer";
-import { NextIntlClientProvider } from 'next-intl';
+import { NextIntlClientProvider } from "next-intl";
 
 import localFont from "next/font/local";
 import "./globals.css";
@@ -28,26 +28,48 @@ export const metadata: Metadata = {
   to children with special needs at the Nutrition Centre— a government- run orphanage in Phnom Penh originally established for
   children aged 0-6 who had been abandoned or lost their families during the Khmer Rouge regime(1975-1979).In offering education
   to this extremely disadvantaged group, The Rabbit School laid the foundation for Cambodia's first formal educational programs for
-  children with disabilities.`
+  children with disabilities.`,
+  openGraph: {
+    title: "Rabbit School Organization",
+    description: `The Rabbit School is a Cambodian NGO ...`,
+    images: [
+      {
+        url: "/images/thumnailOpenGraph.png",
+        width: 1200,
+        height: 630,
+        alt: "Rabbit School Banner",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Rabbit School Organization",
+    description: `The Rabbit School is a Cambodian NGO ...`,
+    images: ["/images/thumnailOpenGraph.png"],
+  },
 };
 
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-}
+};
 
-export default async function LocaleLayout({ children }: {children: React.ReactNode}) {
+export default async function LocaleLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   // Ensure that the incoming `locale` is valid
 
   return (
     <html lang="en">
-      <body className={`${bebasNeue.variable} ${arial.variable} ${playfair.variable} antialiased`}>
+      <body
+        className={`${bebasNeue.variable} ${arial.variable} ${playfair.variable} antialiased`}
+      >
         <NextIntlClientProvider>
           <div className="flex flex-col min-h-screen bg-gray-50">
             <Header />
-              <main className="flex-1">
-                {children}
-              </main>
+            <main className="flex-1">{children}</main>
             <Footer />
           </div>
         </NextIntlClientProvider>
